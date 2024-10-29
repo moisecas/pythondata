b5a99d6b7b8ec6838d7e3a6410f9f45a7b0528b4
mcastro <mcastro@talana.com>
2024-10-28 21:37:33 -0500

    ajuste script para comparar data en excel

--- compare_excel.py
+++ compare_excel.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
 # Definir las rutas de los archivos de Excel
-file1 = 'C:\\Users\\moise\\Downloads\\informe-vacacion-periodo-peru_despues.xls'
-file2 = 'C:\\Users\\moise\\Downloads\\informe-vacacion-periodo-peru_antes.xls'
-
+file1 = 'C:\\Users\\moise\\Downloads\\reporteutilidadesmigra.xlsx'
+file2 = 'C:\\Users\\moise\\Downloads\\reporteutilidadesprod.xlsx'
 
 # Leer los archivos de Excel
 df1 = pd.read_excel(file1, sheet_name=None)
@@ -20,11 +19,23 @@ else:
     for sheet in sheets1:
         data1 = df1[sheet]
         data2 = df2[sheet]
+        
+        # Alinear los DataFrames por índice y columnas
+        data1, data2 = data1.align(data2, join='outer', axis=1)  # Alineación por columnas
+        data1, data2 = data1.align(data2, join='outer', axis=0)  # Alineación por filas
+
+        # Llenar valores NaN con un valor neutral (opcional)
+        data1 = data1.fillna('NaN')
+        data2 = data2.fillna('NaN')
 
         # Comparar los datos de las hojas
         comparison = data1.compare(data2, keep_equal=False)
 
-        # Guardar el reporte de diferencias
-        report_file = f'reporte_diferencias_{sheet}.xlsx'
-        comparison.to_excel(report_file)
-        print(f'Reporte de diferencias guardado en {report_file}')
+        if not comparison.empty:  # Verifica si hay diferencias
+            # Guardar el reporte de diferencias si existen
+            report_file = f'reporte_diferencias_{sheet}.xlsx'
+            comparison.to_excel(report_file)
+            print(f"Diferencias encontradas en la hoja '{sheet}'. Reporte guardado en {report_file}")
+        else:
+            print(f"No se encontraron diferencias en la hoja '{sheet}'.")
+
